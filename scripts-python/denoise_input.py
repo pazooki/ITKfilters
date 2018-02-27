a0c995bd6a169162cbb2e9c6814a11bd78518015
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2018-02-27 21:35:43 +0100

    Add python denoise with CurvatureAnisotropic

--- scripts-python/denoise_input.py
+++ scripts-python/denoise_input.py
@@ -1,39 +1,40 @@
-import prox_tv as ptv
 import sys
 import os
 import itk
 import numpy as np
-import pywt
 
-if len(sys.argv) != 4:
-  print("Usage: " + sys.argv[0] + "inputImage outputFOLDER lambda")
+if len(sys.argv) < 5 or len(sys.argv) > 6:
+  print("Usage: " + sys.argv[0] + "inputImage outputFOLDER numberOfIterations conductance timeStep  ")
   sys.exit(1)
-print("TV denoising %s" % sys.argv[1])
+print("Anisotropic Denoising %s" % sys.argv[1])
 
 input_filename = sys.argv[1]
 basename = os.path.basename(input_filename)
 filename = os.path.splitext(basename)[0]
 output_folder = sys.argv[2]
 output_extension = "nrrd"
-lam = float(sys.argv[3])
-output_filename = os.path.join(output_folder, filename + "_tv" + str(lam) + "." + output_extension)
+iters = int(sys.argv[3])
+conductance = float(sys.argv[4])
+time_step = 1.0/(2.0**4) # 0.0625
+if len(sys.argv) == 5:
+  time_step = float(sys.argv[4])
+output_filename = os.path.join(output_folder, filename +
+                               "_AnisDenoise_c" + str(conductance) +
+                               "_N" + str(iters) +
+                               "_t" + str(time_step) +
+                               "." + output_extension)
 PixelType = itk.F
 Dimension = 3
 ImageType = itk.Image[PixelType, Dimension]
 reader = itk.ImageFileReader[ImageType].New(FileName=input_filename)
 reader.Update()
-image = reader.GetOutput()
 
-image_array = itk.GetArrayViewFromImage(image)
-norm = 1
-dimensions_to_penalyze = [1,2,3]
-tv_array = ptv.tvgen(image_array, np.array([lam,lam,lam]), dimensions_to_penalyze, np.array([norm,norm,norm]))
+denoiser = itk.CurvatureAnisotropicDiffusionImageFilter.New(Input=reader.GetOutput())
+denoiser.Update()
 
-tv_array = np.ascontiguousarray(tv_array, dtype=image_array.dtype)
-modifiedImage = itk.PyBuffer[ImageType].GetImageViewFromArray(tv_array)
-itk.ImageFileWriter.New(Input=modifiedImage, FileName=output_filename).Update()
+itk.ImageFileWriter.New(Input=denoiser.GetOutput(), FileName=output_filename).Update()
 
 from subprocess import Popen
 testDriver = "/home/phc/tmp/IsotropicWaveletsTestDriver"
 pin = Popen([testDriver, 'runViewImage', input_filename, 'input'])
-pout = Popen([testDriver, 'runViewImage', output_filename, 'TV'])
+pout = Popen([testDriver, 'runViewImage', output_filename, 'denoised'])
---
+++ scripts-python/denoise_tv.py
@@ -0,0 +1,39 @@
+import prox_tv as ptv
+import sys
+import os
+import itk
+import numpy as np
+import pywt
+
+if len(sys.argv) != 4:
+  print("Usage: " + sys.argv[0] + "inputImage outputFOLDER lambda")
+  sys.exit(1)
+print("TV denoising %s" % sys.argv[1])
+
+input_filename = sys.argv[1]
+basename = os.path.basename(input_filename)
+filename = os.path.splitext(basename)[0]
+output_folder = sys.argv[2]
+output_extension = "nrrd"
+lam = float(sys.argv[3])
+output_filename = os.path.join(output_folder, filename + "_tv" + str(lam) + "." + output_extension)
+PixelType = itk.F
+Dimension = 3
+ImageType = itk.Image[PixelType, Dimension]
+reader = itk.ImageFileReader[ImageType].New(FileName=input_filename)
+reader.Update()
+image = reader.GetOutput()
+
+image_array = itk.GetArrayViewFromImage(image)
+norm = 1
+dimensions_to_penalyze = [1,2,3]
+tv_array = ptv.tvgen(image_array, np.array([lam,lam,lam]), dimensions_to_penalyze, np.array([norm,norm,norm]))
+
+tv_array = np.ascontiguousarray(tv_array, dtype=image_array.dtype)
+modifiedImage = itk.PyBuffer[ImageType].GetImageViewFromArray(tv_array)
+itk.ImageFileWriter.New(Input=modifiedImage, FileName=output_filename).Update()
+
+from subprocess import Popen
+testDriver = "/home/phc/tmp/IsotropicWaveletsTestDriver"
+pin = Popen([testDriver, 'runViewImage', input_filename, 'input'])
+pout = Popen([testDriver, 'runViewImage', output_filename, 'TV'])
